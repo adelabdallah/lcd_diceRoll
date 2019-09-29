456921c28484781940c2d641a6b913b899421e3e
Adel <adeloabdallah1995@gmail.com>
2019-09-28 21:49:34 -0400

    Hey look it actually works

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-Dice rolling simulator. Prompts user to select a quantity of dice to roll, between 1 and 10.
-Then, prompts user to select the number of sides for the 1st die, either 2, 4, 6, 8, 12, or 20.
-Returns the result of the roll, then prompts for the number of sides of the 2nd die.
-Continues until nth die. 
+User controlled dice rolling simulator. Select number of sides, between 2, 4, 6, 8, 12, 20, shows result, then resets.
--- diceRolling/diceRolling.ino
+++ diceRolling/diceRolling.ino
@@ -1,7 +1,7 @@
-/* User controlled dice rolling simulator. Can roll up to 
-10 dice, with 2, 4, 6, 8, 12, 20 sides. User selects
-amount of dice to roll with buttons, then is prompted for the # of sides of 
-dice x, returns its result, then asks for input of next die's sides.*/
+/*
+    User controlled dice rolling simulator. Select number of sides,
+    between 2, 4, 6, 8, 12, 20, shows result, then resets.
+*/
 
 
 #include <LiquidCrystal.h>
@@ -9,52 +9,56 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int ledPin = 13;
 
-int d_num = 1;
-int sel_state = 0; //reading of selector switch (selects value)
-int right_state = 0; // reading of right switch (increments by +1)
-int left_state = 0; // reading of left switch (increments by -1)
-int dice = 1;
-int sides[] = {2, 4, 6, 8, 12, 20}; 
-int i  = 0; //index for sides[]
+int sel_counter = 0; // how many times select button has been pressed during execution
+int sides[] = {2, 4, 6, 8, 12, 20};
+int i  = 0; // index for sides[]
+int x = 0; // # of sides selected
 
-int dice_sel(){
-  if(digitalRead(7) == HIGH && d_num < 10){
-      d_num += 1; //increments d_num +1 per right switch press, up to 10
-      delay(100); // so button doesn't immediately increment to maximum value
-     
-      }
-      else if(digitalRead(6) == HIGH && d_num > 1){
-      d_num -= 1; //increments d_num -1 per left switch press
-      delay(100); //so button doesn't immediately increment to minimum value
-      
-      }
-    return d_num;
-}
 
-int side_sel(){
-  if(digitalRead(7) == HIGH && i < 5){
-    i += 1;
-    delay(100);
-  }
-  else if(digitalRead(6) == HIGH && i > 0){
-    i -= 1;
-    delay(100);
+int side_sel() {
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Num of sides");
+  lcd.setCursor(0, 1);
+  lcd.print(String(2));
+
+  while (digitalRead(8) == LOW) {
+
+    if (digitalRead(7) == HIGH && i < 5) {
+      i += 1;
+      delay(200);
+    }
+    else if (digitalRead(6) == HIGH && i >= 0) {
+      i -= 1;
+      delay(200);
+    }
+    if (i < 4) { // removes second character from display after select 12 or 20 then going down to 8 or lower
+      lcd.setCursor(1, 1);
+      lcd.print(" ");
+      lcd.setCursor(0, 1);
+    }
+    if (i < 0) {
+      i = 0;
+      }
+    lcd.print(String(sides[i]));
+    lcd.setCursor(0, 1);
   }
   return sides[i];
 }
-  
-int how_many(int x){
-  lcd.setCursor(0,0);
-  lcd.print("How many dice");
-  lcd.setCursor(0,1);
-  lcd.print("to roll: " + String(x));
-  if(x!=10){                //this gets rid of the zero in the (10,1) position on the LCD
-    lcd.setCursor(10,1);    //after de-incrementing from 10
-    lcd.print(' ');
-  }
+
+void result(int x) {
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Result");
+  lcd.setCursor(0, 1);
+  lcd.print(String(random(1, x + 1)));
 }
 
 void setup() {
+
+  Serial.begin(9600);
 
   pinMode(ledPin, OUTPUT);      //using onboard LED to signal beginning
   digitalWrite(ledPin, HIGH);   //and end of dice # selection process
@@ -63,44 +67,30 @@ void setup() {
   pinMode(6, INPUT); // left switch
   pinMode(7, INPUT); // right switch
   pinMode(8, INPUT); // selector switch
-  
-  sel_state = digitalRead(8); //reading of selector switch
-  right_state = digitalRead(7); // reading of right switch
-  left_state = digitalRead(6); // reading of left switch
-  
-  lcd.begin(16, 2); // # of columns & rows on LCD
 
-  while(digitalRead(8) ==  LOW){
-    
-    int x = dice_sel();
-    int y = how_many(x);
-    
-    if(digitalRead(8) == HIGH){
-      break;
-    }
-  }
+  lcd.begin(16, 2); // # of columns & rows on LCD
 
   digitalWrite(ledPin, LOW);
 
+  randomSeed(analogRead(13)); // set random seed based on noise from empty pin
+
   lcd.clear();
-    
-  while(sel_state == LOW){
-    lcd.setCursor(0,0);
-    lcd.print("Sides of die "+ String(dice));
-    
-    lcd.setCursor(0,1);
-    lcd.print(String(side_sel()));
-    if(i < 4){
-      lcd.setCursor(1,1);
-      lcd.print(" ");
-    }
-    
-    delay(100);
+}
+
+void loop() {
+
+  if (sel_counter == 0) {
+    x = side_sel();
+    sel_counter++;
   }
 
+  if (sel_counter == 1) {
+    result(x);
+    sel_counter = 0;
+    x = 0;
+    i = 0;
+  }
+  delay(1500);
+  lcd.clear();
+  randomSeed(analogRead(13));
 }
-
-void loop() { 
-  
-}
-
